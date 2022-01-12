81aab2ffbefd65f15031146d63601db9e8ded446
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2022-01-12 17:36:04 +0100

    revert ratelimiter

--- server/server.js
+++ server/server.js
@@ -26,6 +26,16 @@ app.use(express.static("public"));
 app.get("/isAlive", (req, res) => res.sendStatus(200));
 app.get("/isReady", (req, res) => res.sendStatus(200));
 
+const RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
 app.use(statusRoute);
 
 // catch 404 and forward to error handler
