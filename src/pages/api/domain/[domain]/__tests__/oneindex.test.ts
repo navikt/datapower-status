8b845b13843786856d10c677cf8ab3002939a45f
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2026-08-26 12:01:14 +0200

    invalid dependabot.yml and codeql fix

--- src/pages/api/domain/[domain]/__tests__/oneindex.test.ts
+++ src/pages/api/domain/[domain]/__tests__/oneindex.test.ts
@@ -8,7 +8,7 @@ import { clearJsonCache } from '../../../../../libs/storage';
 jest.mock('@google-cloud/storage');
 
 describe('API domain/[domain] Route', () => {
-  const inputData = '{"testDomain":{"versions":{"dp-01":"2.0.1","dp-02":"2.0.1"}},"testDomain2":{"versions":{"dp-01":"6.6.6"}}}'
+  const inputData = '{"testDomain":{"versions":{"dp-01":"2.0.1","dp-02":"2.0.1"}},"testDomain2":{"versions":{"dp-01":"6.6.6"}}}';
   let mockBucket: Bucket;
   let mockFile: File;
 
